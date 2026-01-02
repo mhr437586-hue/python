ad075a079ad5915b46b9cfeb96703531110897ba
mhr437586-hue <mhr437586@gmail.com>
2026-01-02 12:48:03 +0500

    Update 1st prgoram.py

--- 1st prgoram.py
+++ 1st prgoram.py
@@ -1,3 +1,22 @@
+print("Mehak Nasir")
+print("M.Nasir")
+print("BSCS")
+'''
+ fsc from punjab college
+ matric from Crescent School
+'''
+# BSCS from Gov.t College(pending)
+'''
+want to become a 
+love my parents
+love my ambition
+'''
+print("may allah always bless me ")
+print("May Allah help me what i want ")
+print("Value your time you will be valued","Ameen")
+print("Ameen")
+print(123)
+print (2+3)
 name= "mehak" #string (mix of words in double quotes)
 age= 19
 print(name)
@@ -5,4 +24,5 @@ print(age)
 print("My name is:", name)
 print("My age is :",age)
 
-print("Hi")
+
+
